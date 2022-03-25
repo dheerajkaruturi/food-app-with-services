67c4bda14040356815b4090d2f0808dc68045a04
dheeraj karuturi <dheeraj.karuturi@audreeinfotech.com>
2022-03-25 09:42:52 +0530

    changes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,6 +15,7 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
+    
     this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
   ingredientsChanged = new EventEmitter<Ingredient[]>();
+  
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
